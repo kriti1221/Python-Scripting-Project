2b167b80d9433099247609f966befacb2efb32ab
kriti <kriti.jindal.37@gmail.com>
2024-02-15 16:46:34 +0530

    compiling and running go code

--- get_game_data.py
+++ get_game_data.py
@@ -55,31 +55,32 @@ def make_json_metadata_file(path, game_dirs):
         json.dump(data, f)
 
 
-# def compile_game_code(path):
-#     code_file_name = None
-#     for root, dirs, files in os.walk(path):
-#         for file in files:
-#             if file.endswith(GAME_CODE_EXTENSION):
-#                 code_file_name = file
-#                 break
+def compile_game_code(path):
+    code_file_name = None
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(GAME_CODE_EXTENSION):
+                code_file_name = file
+                print("Found code file", code_file_name)
+                break
 
-#         break
+        break
 
-#     if code_file_name is None:
-#         return
+    if code_file_name is None:
+        return
 
-#     command = GAME_COMPILE_COMMAND + [code_file_name]
-#     run_command(command, path)
+    command = GAME_COMPILE_COMMAND + [code_file_name]
+    print("final",command)
+    run_command(command, path)
 
+def run_command(command, path):
+    cwd = os.getcwd()
+    os.chdir(path)
+    print("command",command)
+    result = run(command, stdout=PIPE, stdin=PIPE, universal_newlines=True)
+    print("compile result", result)
 
-# def run_command(command, path):
-#     cwd = os.getcwd()
-#     os.chdir(path)
-
-#     result = run(command, stdout=PIPE, stdin=PIPE, universal_newlines=True)
-#     print("compile result", result)
-
-#     os.chdir(cwd)
+    os.chdir(cwd)
 
 def main(source, target):
     cwd = os.getcwd()
@@ -94,10 +95,10 @@ def main(source, target):
     for src, dest in zip(game_paths, new_game_dirs):
         dest_path = os.path.join(target_path, dest)
         copy_and_overwrite(src, dest_path)
-    #     compile_game_code(dest_path)
+        compile_game_code(dest_path)
 
-    # json_path = os.path.join(target_path, "metadata.json")
-    # make_json_metadata_file(json_path, new_game_dirs)
+    json_path = os.path.join(target_path, "metadata.json")
+    make_json_metadata_file(json_path, new_game_dirs)
 
 
 if __name__ == "__main__":
