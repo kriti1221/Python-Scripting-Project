c3b9e05b77c3ea4c7a377f86a41f3cce21664bca
kriti <kriti.jindal.37@gmail.com>
2024-02-15 14:39:28 +0530

    trying fork

--- get_game_data.py
+++ get_game_data.py
@@ -24,14 +24,14 @@ def find_all_game_paths(source):
     return game_paths
 
 
-def get_name_from_paths(paths, to_strip):
-    new_names = []
-    for path in paths:
-        _, dir_name = os.path.split(path)
-        new_dir_name = dir_name.replace(to_strip, "")
-        new_names.append(new_dir_name)
+# def get_name_from_paths(paths, to_strip):
+#     new_names = []
+#     for path in paths:
+#         _, dir_name = os.path.split(path)
+#         new_dir_name = dir_name.replace(to_strip, "")
+#         new_names.append(new_dir_name)
 
-    return new_names
+#     return new_names
 
 
 def create_dir(path):
@@ -87,17 +87,17 @@ def main(source, target):
     target_path = os.path.join(cwd, target)
 
     game_paths = find_all_game_paths(source_path)
-    new_game_dirs = get_name_from_paths(game_paths, "_game")
+    # new_game_dirs = get_name_from_paths(game_paths, "_game")
 
     create_dir(target_path)
 
-    for src, dest in zip(game_paths, new_game_dirs):
-        dest_path = os.path.join(target_path, dest)
-        copy_and_overwrite(src, dest_path)
-        compile_game_code(dest_path)
+    # for src, dest in zip(game_paths, new_game_dirs):
+    #     dest_path = os.path.join(target_path, dest)
+    #     copy_and_overwrite(src, dest_path)
+    #     compile_game_code(dest_path)
 
-    json_path = os.path.join(target_path, "metadata.json")
-    make_json_metadata_file(json_path, new_game_dirs)
+    # json_path = os.path.join(target_path, "metadata.json")
+    # make_json_metadata_file(json_path, new_game_dirs)
 
 
 if __name__ == "__main__":
